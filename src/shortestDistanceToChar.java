6ca0e797f673072c189294a66ae989ebe0f4051a
Sean McGarry <shan468@hotmail.com>
2022-11-29 22:34:52 +0000

    Shortest Distance to Character 1/2

---
+++ src/shortestDistanceToChar.java
@@ -0,0 +1,25 @@
+
+
+public class shortestDistanceToChar {
+    public static int[] shortestDistanceToChar(String st, char c) {
+
+        int[] answers = new int[st.length()];
+
+        char[] charArray = st.toCharArray();
+
+        for (int i = 0; i < charArray.length; i++) {
+            for (int j = 0; j < charArray.length; j++) {
+                int counter = 0;
+                if (charArray[j] != c) {
+                    counter++;
+                } else {
+                    answers[i] = counter;
+                }
+
+            }
+
+        }
+
+        return answers;
+    }
+}
